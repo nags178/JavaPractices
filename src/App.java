b8c453e0f100a168e487c40fffa61ba8cd879fc3
nags178 <nags17.8@gmail.com>
2021-01-02 10:20:42 +0530

    Pyramid

--- src/App.java
+++ src/App.java
@@ -12,17 +12,19 @@ public class App {
         System.out.println("Hello, World! and i value"+appObj.i);
         System.out.println();
         Scanner scannerObj = new Scanner(System.in);
-        Scanner scanObj = new Scanner("Hi this is nag");
-      //  System.out.println("Enter my input: ");
-     //   int i = scannerObj.nextInt();
+       //// String input = "Hi this is nag";
+      //  Scanner scanObj = new Scanner(input);
+        System.out.println("Enter my input: ");
+        int i = scannerObj.nextInt();
+       // int j = scannerObj.nextInt();
+      //  String str = scanObj.next();
         
-        String str = scanObj.nextLine();
         
-       // String str1 = scannerObj.next();
-       // String str2 = scannerObj.next();
-        System.out.println("My entered input is: " +str);
+        //String str1 = scannerObj.nextLine();
+        //String str2 = scannerObj.next();
+        System.out.println("My entered input is: " +i);
         scannerObj.close();
-        scanObj.close();
+       // scanObj.close();
        //MainClass obj = new MainClass();
     }
     
